2f6f15afbfe33b2e4811c13d7aab86024c948afc
takara.miyazaki <takara.miyazaki@rakus.co.jp>
2019-05-28 17:59:25 +0900

    従業員詳細画面の実装

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.Module.SetupContext;
+
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,6 +27,15 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	/**
+	 * UpdateEmployeeFormをインスタンス化
+	 * @return UpdateEmployeeForm
+	 */
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 	
 	/**
 	 * 従業員一覧を出力する.
@@ -36,5 +49,19 @@ public class EmployeeController {
 		model.addAttribute("employeeList", employeeList);
 		
 		return "employee/list";
+	}
+	
+	/**
+	 * 従業員の詳細情報表示.
+	 * 
+	 * @param id　従業員ID
+	 * @param model リクエストスコープ
+	 * @return　従業員の詳細情報画面
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 }
